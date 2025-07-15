596f37f3a940d4237b1ca352852a1de405c6a621
Majesty Yolo <145739768+Octo-Data-Scientist@users.noreply.github.com>
2025-07-15 23:31:15 +0100

    Add files via upload

---
+++ Lectures/Lecture 0 - Querying/src0/1-LIMIT.sql
@@ -0,0 +1,7 @@
+-- Limit results to first 3
+SELECT "title", "author" FROM "longlist" LIMIT 3;
+
+-- Limit results to first 10
+SELECT "title", "author" FROM "longlist" LIMIT 10;
+
+SELECT "title", "author", "publisher" FROM longlist LIMIT 20;
