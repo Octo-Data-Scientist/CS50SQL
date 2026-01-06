c92949bb60fee64a80e7c7771147f1010c075767
Majesty Yolo <145739768+Octo-Data-Scientist@users.noreply.github.com>
2026-01-06 20:14:01 +0100

    Add files via upload

--- Lectures/Lecture 1 - Relating/joins.sql
+++ Lectures/Lecture 1 - Relating/joins.sql
@@ -1,28 +1,30 @@
 -- sea lion table
-SELECT * FROM sea_lions;
+SELECT * FROM "sea_lions";
 
 -- migrations table
-SELECT * FROM migrations;
+SELECT * FROM "migrations";
 
 -- Applying joins to the sea lions and migration tables
-SELECT * FROM sea_lions JOIN migrations 
+SELECT * FROM "sea_lions" JOIN "migrations" 
 ON "migrations".id = "sea_lions".id;
 
 -- LEFT JOIN
-SELECT * FROM sea_lions LEFT JOIN migrations
+SELECT * FROM "sea_lions" LEFT JOIN "migrations"
  ON "migrations".id = "sea_lions".id;
 
 --  RIGHT JOIN
-SELECT * FROM sea_lions RIGHT JOIN migrations 
+SELECT * FROM "sea_lions" RIGHT JOIN "migrations" 
 ON "migrations".id = "sea_lions".id;
 
 
 -- FULL JOINS
-SELECT * FROM sea_lions FULL JOIN migrations 
+SELECT * FROM "sea_lions" FULL JOIN "migrations" 
 ON "migrations".id = "sea_lions".id;
 
 -- NATURAL JOIN
-SELECT * FROM sea_lions NATURAL JOIN migrations;
+-- Natural join is just like join but the only difference
+-- is that the id column appears once.
+SELECT * FROM "sea_lions" NATURAL JOIN "migrations";
 
-SELECT * FROM sea_lions JOIN migrations
+SELECT * FROM "sea_lions" JOIN "migrations"
 ON "migrations".id = "sea_lions".id WHERE "migrations".distance > 1500;
