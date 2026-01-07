71fb97f0c92f6984957513fb14c5f6f1b086146f
Majesty Yolo <145739768+Octo-Data-Scientist@users.noreply.github.com>
2026-01-07 05:12:06 +0100

    Add files via upload

---
+++ Lectures/Lecture 1 - Relating/sets.sql
@@ -0,0 +1,57 @@
+-- Sets
+-- UNION
+-- Selecting all authors, labeling as authors
+SELECT 'author' AS "Profession", "name" FROM "authors";
+
+-- Selecting all translators, labeling as translators
+SELECT 'translator' AS "Profession", "name" FROM "translators";
+
+-- Combine authors and translators into one result set (removes duplicates).
+SELECT 'author' AS "Profession", "name" FROM "authors"
+UNION
+SELECT 'translator' AS "Profession", "name" FROM "translators";
+
+-- Combine authors and translators into one result set (leaves duplicates).
+SELECT 'author' AS "Profession", "name" FROM "authors"
+UNION ALL
+SELECT 'translator' AS "Profession", "name" FROM "translators";
+
+-- INTERSECT
+-- Assume names are unique
+
+-- Find authors and translators
+SELECT "name" FROM "authors"
+INTERSECT
+SELECT "name" FROM "translators";
+
+-- Find books translated by Sophie Hughes
+SELECT "book_id" FROM "translated" WHERE "translator_id" = (
+    SELECT "id" FROM "translators" WHERE "name" = 'Sophie Hughes'
+);
+
+-- -- Find books translated by Margaret Jull Costa
+SELECT "book_id" FROM "translated" WHERE "translator_id" = (
+    SELECT "id" FROM "translators" WHERE "name" = 'Margaret Jull Costa'
+);
+
+-- -- Find Intersection of books
+SELECT "book_id" FROM "translated" WHERE "translator_id" = (
+    SELECT "id" FROM "translators" WHERE "name" = 'Sophie Hughes'
+)
+INTERSECT
+SELECT "book_id" FROM "translated" WHERE "translator_id" = (
+    SELECT "id" FROM "translators" WHERE "name" = 'Margaret Jull Costa'
+);
+
+SELECT "title" FROM "books" WHERE "id" IN(
+    SELECT "book_id" FROM "translated" WHERE "translator_id" = (
+        SELECT "id" FROM "translators" WHERE "name" = 'Sophie Hughes'
+    )
+);
+
+
+
+
+
+
+
